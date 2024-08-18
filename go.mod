aeb5240cb607738bfce729a400b31e4f9c24e7ee
Ondřej Michálek <michondr@gmail.com>
2024-08-18 18:43:29 +0200

    parse toggl issues

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module toggl-jira-worklogs
 
 go 1.22
+
+require (
+	github.com/jason0x43/go-toggl v0.0.0-20240528025633-4e5873a36db2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,114 @@
 package main
 
-func main() {
+import (
+	"encoding/base64"
+	"fmt"
+	"github.com/jason0x43/go-toggl"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
 
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	token_jira := os.Getenv("TOKEN_JIRA")
+	if token_jira == "" {
+		panic("missing TOKEN_JIRA")
+	}
+	token_toggl := os.Getenv("TOKEN_TOGGL")
+	if token_toggl == "" {
+		panic("missing TOKEN_TOGGL")
+	}
+
+	authToken := base64.StdEncoding.Strict().EncodeToString([]byte("michondr:" + token_toggl))
+	fmt.Println(authToken)
+
+	toggl.EnableLog()
+	session := toggl.OpenSession(token_toggl)
+
+	lastWorklogDateInJira := time.Date(2024, 8, 1, 0, 0, 0, 0, time.UTC)
+	now := time.Now()
+
+	relevantEntries, err := session.GetTimeEntries(lastWorklogDateInJira, now)
+	if err != nil {
+		panic("cannot get time entries: " + err.Error())
+	}
+
+	const (
+		PROJECT_MEETING     = 202187965
+		PROJECT_CODE_REVIEW = 202268075
+		PROJECT_OPERATIVE   = 204586275
+		PROJECT_FEATURE     = 202217954
+	)
+
+	projectIdToProject := map[int]toggl.Project{
+		PROJECT_CODE_REVIEW: {Name: "cr"},
+		PROJECT_MEETING:     {Name: "meetings"},
+		PROJECT_OPERATIVE:   {Name: "někdo po mě něco chce"},
+		PROJECT_FEATURE:     {Name: "feature"},
+	}
+
+	type jiraIssue struct {
+		ID string
+	}
+	scrumIssue := jiraIssue{ID: "REC-3333"} //TODO: what's scrum ID
+
+	type jiraTimeEntry struct {
+		Issue       jiraIssue
+		Description string
+		From        *time.Time
+		To          *time.Time
+	}
+	issuesToinsertTojira := []jiraTimeEntry{}
+
+	for _, entry := range relevantEntries {
+		projectId := *entry.Pid
+
+		_, exists := projectIdToProject[projectId]
+		if !exists {
+			panic("project " + string(rune(projectId)) + " not found in array: " + err.Error())
+		}
+
+		isREC := strings.HasPrefix(entry.Description, "REC-")
+		hasDescription := len(entry.Description) > 8
+
+		if isREC {
+			descr := "code review"
+			if hasDescription {
+				descr = entry.Description[11:]
+			}
+
+			issuesToinsertTojira = append(
+				issuesToinsertTojira,
+				jiraTimeEntry{
+					Issue:       jiraIssue{entry.Description[:8]},
+					Description: descr,
+					From:        entry.Start,
+					To:          entry.Stop,
+				},
+			)
+
+			continue
+		}
+
+		issuesToinsertTojira = append(
+			issuesToinsertTojira,
+			jiraTimeEntry{
+				Issue:       scrumIssue,
+				Description: entry.Description,
+				From:        entry.Start,
+				To:          entry.Stop,
+			},
+		)
+	}
+
+	for _, issue := range issuesToinsertTojira {
+		fmt.Println(issue)
+	}
 }
